360a195dc52db57523309621d06a9f0b7a8a8333
Sahil Modi <Sahil.Modi1@BRILLIO.COM>
2024-05-29 14:46:41 +0530

    Myfirstcommit

---
+++ src/main/java/com/example/JenkisProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkisProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkisProjectApplication.class, args);
+	}
+
+}
